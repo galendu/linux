3e938a690166a17ff30c7f50abe480baad1b82a8
spider <spider@duxiao.me>
2020-07-08 14:44:19 +0800

    Create docker_install.sh

---
+++ centos/docker/docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo  https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum install -y docker-ce-18.06.0.ce-3.el7 
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "graph": "/data/docker",
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+  "max-size": "100m" },
+  "storage-driver": "overlay2"
+}
+EOF
+systemctl daemon-reload
+
+systemctl enable docker
