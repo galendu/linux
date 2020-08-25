df13fab955d636b84d3c9f8dd50e548e2b4ff265
spider <spider@duxiao.me>
2020-08-26 07:16:03 +0800

    Create sed-i-e.sh
    sed利用数组和-e参数替换多个匹配项

---
+++ shell/sed-i-e.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex # -e 遇到执行非零时，退出脚本 -x打印执行过程
+cat a.txt |while read -a LINE; #读取文件的每行，并以数组的形式保存在LINE中
+do cp a.yaml ${LINE[0]}.yaml && sed -i -e  s%app%${LINE[0]}%g -e  s%port%${LINE[1]}%g ${LINE[0]}.yaml; #替换文件中匹配的项
+done
