2201e0248021b3bd1375f1f7658a3f67d1f31888
spider <spider@duxiao.me>
2020-10-07 12:37:53 +0800

    Update docker_install.sh

--- centos/docker/docker_install.sh
+++ centos/docker/docker_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo  https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum install -y docker-ce-18.06.0.ce-3.el7 
+yum install -y docker-ce-19.03.9.ce-3.el7 
 mkdir -p /etc/docker
 tee /etc/docker/daemon.json <<-'EOF'
 {
