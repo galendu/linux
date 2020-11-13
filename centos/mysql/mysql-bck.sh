1df76057b6f05db7adf02de1ed99323dedd1d1ff
spider <spider@duxiao.me>
2020-11-13 20:01:58 +0800

    Create mysql-bck.sh

---
+++ centos/mysql/mysql-bck.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /bck/data/ -name *.tar.gz -mtime 30 |xargs rm
+mysqldump -h 192.168.0.x -P 13306 -uroot -p'123456' --all-databases |gzip >/bck/data/dev-k8s-`date +%Y%m%d%H%M`.tar.gz
