75d42235d72838e51442c8cb48c8396a067d2816
spider <spider@duxiao.me>
2020-11-15 11:23:11 +0800

    Create entrypoint.sh

---
+++ centos/docker/server/Dockerfile-nginx/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+cat > /etc/nginx/conf.d/www.conf << EOF
+{
+    server_name $HOSTNAME;
+    listen ${ip:-0.0.0.0}:${PORT:-80};
+    root ${NGX_DOC_ROOT:-/usr/share/nginx/html};
+}
+EOF
