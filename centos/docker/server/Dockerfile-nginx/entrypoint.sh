642cd93ae3c66e2ac8acefd0fd62fc1fd4330fc6
spider <spider@duxiao.me>
2020-11-15 11:37:39 +0800

    Update entrypoint.sh

--- centos/docker/server/Dockerfile-nginx/entrypoint.sh
+++ centos/docker/server/Dockerfile-nginx/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 cat > /etc/nginx/conf.d/www.conf << EOF
-{
-    server_name $HOSTNAME;
+server {
+    server_name ${HOSTNAME};
     listen ${ip:-0.0.0.0}:${PORT:-80};
     root ${NGX_DOC_ROOT:-/usr/share/nginx/html};
 }
