42caf7808e70c9cc907d5ce3cde63e218519d439
jafardu <spider@duxiao.me>
2021-04-22 15:28:30 +0800

    Update youhua.sh

--- debian/debian/youhua.sh
+++ debian/debian/youhua.sh
@@ -1,9 +1,11 @@
+#解决https报错问题
 apt install apt-transport-https ca-certificates
 # buster版本
 # https://mirrors.tuna.tsinghua.edu.cn/help/debian/
 # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 cp /etc/apt/sources.list  /etc/apt/sources.list.bck
 
+# 清华源
 cat >/etc/apt/sources.list<-eof
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
