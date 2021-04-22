bb4eae39f15a0a6bf2aeefac305cf48e996110bc
jafardu <spider@duxiao.me>
2021-04-22 15:22:42 +0800

    Update youhua.sh

--- debian/debian/youhua.sh
+++ debian/debian/youhua.sh
@@ -3,7 +3,35 @@ apt install apt-transport-https ca-certificates
 # https://mirrors.tuna.tsinghua.edu.cn/help/debian/
 # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 cp /etc/apt/sources.list  /etc/apt/sources.list.bck
+
 cat >/etc/apt/sources.list<-eof
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free
+eof
+
+#备用 
+--------------------------------------------------------------
+#备用阿里云http
+cat >/etc/apt/sources.list <-eof
+deb http://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb http://mirrors.aliyun.com/debian-security buster/updates main
+deb-src http://mirrors.aliyun.com/debian-security buster/updates main
+deb http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
+eof
+#备用阿里云https
+
+cat >/etc/apt/sources.list <-eof
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free
