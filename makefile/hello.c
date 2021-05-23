3110136ccb88d5f12091cb8c833a3fdb09cf8e7c
jafardu <1610469455@qq.com>
2021-05-23 12:54:01 +0800

    Create hello.c

---
+++ makefile/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  //标准输入输出头文件
+
+#define STR "spider@duxiao.me\n" //宏定义
+#define PRI printf
+
+int main() //主函数
+{
+    PRI(STR);
+    getchar();
+    return 0;
+}
+
+// hello world
