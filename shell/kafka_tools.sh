845332f45f8159fb89eaf9ffed40c2a459aea5cb
Galendu <1610469455@qq.com>
2023-05-14 11:39:12 +0800

    Update kafka_tools.sh

--- shell/kafka_tools.sh
+++ shell/kafka_tools.sh
@@ -3,12 +3,33 @@
 
 HOST_LIST="172.16.100.40 172.16.100.50 172.16.100.60"
 
+# STATUS_CMD="jps|grep -w Kafka"
+# START_CMD="/opt/source/kafka/bin/kafka-server-start.sh --daemon /opt/source/kafka/config/server.properties"
+# STOP_CMD="/opt/source/kafka/bin/kafka-server-stop.sh"
+# SERVICE_NAME="Kafka broker"
+
+# STATUS_CMD="jps|grep -w QuorumPeerMain"
+# START_CMD="/opt/source/zookeeper/bin/zkServer.sh start"
+# STOP_CMD="/opt/source/zookeeper/bin/zkServer.sh stop"
+# SERVICE_NAME="ZK Server"
+
+SLEF_PID=$$
+STATUS_CMD="ps -ef|grep nginx|grep -v grep|grep -v $SLEF_PID"
+START_CMD="/usr/sbin/nginx"
+STOP_CMD="/usr/sbin/nginx -s stop"
+SERVICE_NAME="Nginx"
+
+# :.,$/s/^/#
+# sh -x kafka_tools.sh start
+# watch -n 1 -d "jps"
+# mv kafka-server-start.sh ../
+
 function service_status
 {
         status_idx=0
         result=0
         while [ $status_idx -lt 3 ];do
-                ssh -o StrictHostKeyChecking=no $1 "jps|grep -w Kafka" &> /dev/null
+                ssh -o StrictHostKeyChecking=no $1 $STATUS_CMD &> /dev/null
                 if [ $? -eq 0 ];then
                         result=`expr $result + 1`
                 fi
@@ -24,29 +45,29 @@ function service_status
 function service_start
 {
         for host in $HOST_LIST;do
-                echo "---------Now Begin To start Kafka In Host: $host---------"
+                echo "---------Now Begin To start $SERVICE_NAME In Host: $host---------"
                 service_status $host
                 if [ $? -eq 0 ];then
-                        echo "Kafka broker in $host is already Running"
+                        echo "$SERVICE_NAME in $host is already Running"
                 else
-                        echo "Now Kafka broker is STOPPED,Start it...."
-                        ssh -o StrictHostKeyChecking=no $host "/opt/source/kafka/bin/kafka-server-start.sh --daemon /opt/source/kafka/config/server.properties" &> /dev/null
+                        echo "Now $SERVICE_NAME is STOPPED,Start it...."
+                        ssh -o StrictHostKeyChecking=no $host $START_CMD &> /dev/null
                         index=0
                         while [ $index -lt 5 ];do
                                 service_status $host
                                 if [ $? -ne 0 ];then
                                         index=`expr $index+1`
-                                        echo "$index Times: Kafka broker  in $host start failed... Please wait..."
-                                        echo "After 3 seconds will check kafka status again..."
+                                        echo "$index Times: $SERVICE_NAME  in $host start failed... Please wait..."
+                                        echo "After 3 seconds will check $SERVICE_NAME status again..."
                                         sleep 3
                                         continue
                                 else
-                                        echo "OK...Kafka broker in $host is RUNNING..."
+                                        echo "OK...$SERVICE_NAME in $host is RUNNING..."
                                         break
                                 fi
                         done
                         if [ $index -eq 5 ];then
-                                echo "Sorry...Kafka broker Start Failed..Please login $host to check"
+                                echo "Sorry...$SERVICE_NAME Start Failed..Please login $host to check"
                         fi
                 fi
         done
@@ -55,29 +76,29 @@ function service_start
 function service_stop
 {
         for host in $HOST_LIST;do
-                echo "---------Now Begin To Stop Kafka In Host: $host---------"
+                echo "---------Now Begin To Stop $SERVICE_NAME In Host: $host---------"
                 service_status $host
                 if [ $? -ne 0 ];then
-                        echo "Kafka broker in $host is already STOPPED"
+                        echo "$SERVICE_NAME in $host is already STOPPED"
                 else
-                        echo "Now Kafka broker is RUNNING,Stop it...."
-                        ssh -o StrictHostKeyChecking=no $host "/opt/source/kafka/bin/kafka-server-stop.sh" &> /dev/null
+                        echo "Now $SERVICE_NAME is RUNNING,Stop it...."
+                        ssh -o StrictHostKeyChecking=no $host $STOP_CMD &> /dev/null
                         index=0
                         while [ $index -lt 5 ];do
                                 service_status $host
                                 if [ $? -eq 0 ];then
                                         index=`expr $index+1`
-                                        echo "$index Times: Kafka broker  in $host is stopping... Please wait..."
-                                        echo "After 3 seconds will check kafka status again..."
+                                        echo "$index Times: $SERVICE_NAME  in $host is stopping... Please wait..."
+                                        echo "After 3 seconds will check $SERVICE_NAME status again..."
                                         sleep 3
                                         continue
                                 else
-                                        echo "OK...Kafka broker in $host is STOPPED now..."
+                                        echo "OK...$SERVICE_NAME in $host is STOPPED now..."
                                         break
                                 fi
                         done
                         if [ $index -eq 5 ];then
-                                echo "Sorry...Kafka broker Stop Failed..Please login $host to check"
+                                echo "Sorry...$SERVICE_NAME Stop Failed..Please login $host to check"
                         fi
                 fi
         done
@@ -86,9 +107,9 @@ function service_stop
 function usage
 {
 cat << EOF
-sh $0 start             # Start Kafka Process Define IN HOST_LIST
-sh $0 stop              # Stop Kafka Process Define IN HOST_LIST
-sh $0 status            # Get Kafka Process Define IN HOST_LIST
+sh $0 start             # Start $SERVICE_NAME Process Define IN HOST_LIST
+sh $0 stop              # Stop $SERVICE_NAME Process Define IN HOST_LIST
+sh $0 status            # Get $SERVICE_NAME Process Define IN HOST_LIST
 EOF
 }
 case $1 in
@@ -100,12 +121,12 @@ case $1 in
                 ;;
         status)
                 for host in $HOST_LIST;do
-                        echo "---------Now Begin To Detect Kafka Status In Host: $host---------"
+                        echo "---------Now Begin To Detect $SERVICE_NAME Status In Host: $host---------"
                         service_status $host
                         if [ $? -eq 0 ];then
-                                echo "Kafka broker in $host is RUNNING"
+                                echo "$SERVICE_NAME in $host is RUNNING"
                         else
-                                echo "KAfka broker in $host is STOPPED"
+                                echo "$SERVICE_NAME in $host is STOPPED"
                         fi
                 done
                 ;;
@@ -113,7 +134,3 @@ case $1 in
                 usage
                 ;;
 esac
-# :.,$/s/^/#
-# sh -x kafka_tools.sh start
-# watch -n 1 -d "jps"
-# mv kafka-server-start.sh ../
